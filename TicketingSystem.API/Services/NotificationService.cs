57aad849c415f79ac3648f836609f00fac329ea7
jenusinfo <info@jenus.com.cy>
2025-08-03 07:52:51 +0300

    ok

--- TicketingSystem.API/Services/NotificationService.cs
+++ TicketingSystem.API/Services/NotificationService.cs
@@ -3,5 +3,9 @@ public class NotificationService : INotificationService
     public Task<IEnumerable<NotificationDto>> GetAllAsync(ClaimsPrincipal user) =>
         Task.FromResult<IEnumerable<NotificationDto>>(new List<NotificationDto>());
 
+    // Explicit implementation for the interface method with Task return type (no generic)
+    Task INotificationService.GetAllAsync(ClaimsPrincipal user) =>
+        Task.FromResult<IEnumerable<NotificationDto>>(new List<NotificationDto>());
+
     public Task<bool> MarkAsReadAsync(long id, ClaimsPrincipal user) => Task.FromResult(true);
 }
